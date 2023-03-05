70219299c16aecab91b08e1d0a27a0090eb83ddf
unknown <ibrahim.a.a.h.2017@gmail.com>
2023-03-05 06:31:53 +0200

    A

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css'
+import SingleCard from './components/SingleCard';
 
 const cardImages = [
   {"src":"/img/helmet-1.png"},
@@ -14,6 +15,9 @@ function App()
 {
   const [cards,setCards] = useState([]);
   const [turns,setTurns] = useState(0);
+
+  const [choiceOne,setChoiceOne] = useState(null);
+  const [choiceTwo,setChoiceTwo] = useState(null);
 
   const random_shuffle = () => Math.random() - 0.5;
   const generate_random_id = () => (Math.floor(Math.random() * 1000));
@@ -36,11 +40,46 @@ function App()
     setTurns(0);
   }
 
+  const compare = (card_choice_one , card_choice_two) =>
+  {
+    if (card_choice_one.src === card_choice_two.src) {
+      return 'match';
+    }else{
+      return 'not match';
+    }
+  }
 
+  const handleChoice = (card) => 
+  {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+
+    console.log(choiceOne)
+    // if(choiceOne && choiceTwo) {
+    //   let result = compare(choiceOne,choiceTwo);
+    //   console.log(result)
+    // }
+  }
+
+  const resetTurns = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns => prevTurns + 1));
+  }
   return (
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
+      <div className='card-grid'>
+        {
+          cards.map(card => (
+           <SingleCard 
+              key={card.id} 
+              card={card}
+              handleChoice={handleChoice}
+           />
+          ))
+        }
+      </div>
     </div>
   );
 }
---
+++ src/components/SingleCard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './SingleCard.css'
+
+function SingleCard({card,handleChoice}) {
+
+  const handleClick = () => {
+    // console.log(e.target)
+    handleChoice(card);
+  }
+  return (
+    <div className='card'>
+        <div>
+        <img className='front' src={card.src} alt='card front'/>
+        <img 
+            className='back' 
+            src='/img/cover.png' 
+            alt='card back'
+            onClick={handleClick}
+        />
+        </div>
+    </div>
+  )
+}
+
+export default SingleCard
